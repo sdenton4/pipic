5413ea574178d8a2fcf574a52424edbb923c9d79
kaibutsu <kaibutsu@sophos.(none)>
2014-03-15 15:54:35 -0400

    more json magic

--- djpilapse/djpilapp/static/js/pilapse.js
+++ djpilapse/djpilapp/static/js/pilapse.js
@@ -39,7 +39,7 @@
             $.ajax(url).done( function(){
                 $('#imageFrame').fadeTo('fast', 0.5);
                 setTimeout(function(){
-                    functionStack.push( loadImage );
+                    functionStack.push( [ loadImage, [] ] );
                 },waittime);
             });
         });
