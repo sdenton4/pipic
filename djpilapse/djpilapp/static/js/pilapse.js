2a9a705f0d8a0198b1b3c87c559acc058fba9fcd
sdenton4 <sdenton4@gmail.com>
2014-04-01 22:04:51 -0400

    Fixed image loading

--- djpilapse/djpilapp/static/js/pilapse.js
+++ djpilapse/djpilapp/static/js/pilapse.js
@@ -18,12 +18,12 @@
     }
 
     function updateArticle(page){
+    //This is cruft now.  Delete this function.
       path = baseurl()+ 'djpilapp/' + page +'/';
       console.log(path);
       $.get(path, function(data){
         $('article').html( data );
       });
-      ;
     }
 
     function baseurl() {
@@ -55,8 +55,7 @@
         $('.refreshButton').click(function(){
             functionStack.push( loadImage );
         });
-        
-        
+
         $('#overviewBtn').click(function(){
             event.preventDefault();
             $('#newProject').hide();
@@ -81,8 +80,7 @@
             if (functionStack.length>0) {
                 f = functionStack.pop();
                 //console.log(f);
-                //f();
-                
+                f();
             }
             else {
                 path=baseurl()+'djpilapp/jsonupdate/';
