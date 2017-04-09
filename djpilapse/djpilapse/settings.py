6e347f7f1981d8d655f9357e879044cee2ab0776
tom denton <sdenton4@gmail.com>
2017-04-09 10:48:59 -0700

    Update settings.py

--- djpilapse/djpilapse/settings.py
+++ djpilapse/djpilapse/settings.py
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/pi/pipic/djpilapse/templates/'
+    '/home/pi/pipic/djpilapse/templates/',
 )
 
 
